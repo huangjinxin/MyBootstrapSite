b9b0c7a74c59308a5ded73792da66f1b2cfe34be
dahuang2b <hjx3284@163.com>
2023-12-27 20:24:47 +0800

    add html

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>老徐折腾🤣傻逼</title>
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<?php include 'navbar.html'; ?>
+    <?php include 'carousel.html'; ?>
+    <?php include 'card.html'; ?>
+    <?php include 'info.html'; ?>
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
