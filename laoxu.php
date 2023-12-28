4219a310d95f723456280c957d7c746047a3e8b4
dahuang2b <hjx3284@163.com>
2023-12-28 09:53:38 +0800

    php-laoxu

--- laoxu.php
+++ laoxu.php
@@ -6,23 +6,12 @@
     <title>老徐折腾🤣傻逼</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="xu/balance/css/balance.css">
 </head>
 <body>
 
 <?php include 'navbar.html'; ?>
-
-<?php
-$directories = glob('xu/*', GLOB_ONLYDIR);
-foreach ($directories as $directory) {
-    // 遍历每个目录中的HTML文件
-    foreach (glob($directory . "/*.html") as $htmlFile) {
-        // 为每个HTML文件设置基路径
-        $basePath = $directory . '/'; // 确保这是相对于网站根的路径
-        // 然后包含HTML文件
-        include $htmlFile;
-    }
-}
-?>
+<?php include 'xu/balance/balance.html';?>
 
 <?php include 'info.html'; ?>
 
